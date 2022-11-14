41e2dd62c0f82e8af7eb281fcbe564fc287692fc
Asiia <110309169+Asiiaa@users.noreply.github.com>
2022-11-14 13:12:34 +0200

    Add files via upload

---
+++ HomeWork_18/controller/BoardController.js
@@ -0,0 +1,50 @@
+class BoardController {
+    #boardView = null;
+    #boardModell = null;
+    #boardFormView = null;
+
+    constructor(container) {
+        this.#boardView = new BoardView({
+            onDelete: (id) => this.delete(id),
+            onEdit: (id, value) => this.edit(id, value),
+        });
+        container.append(this.#boardView.el);
+
+        this.#boardModell = new BoardModel();
+        this.#boardModell
+            .fetchBoard()
+            .then(() =>
+                this.#boardView.renderList(this.#boardModell.stickers)
+            );
+
+        this.#boardFormView = new BoardFormView({
+            onCreate: (stickers) => this.create(stickers),
+        });
+        container.prepend(this.#boardFormView.elForm)
+
+    }
+
+    delete(id) {
+        this.#boardModell
+            .delete(id)
+            .then(() =>
+                this.#boardView.renderList(this.#boardModell.stickers)
+            );
+    }
+
+    create(stickers) {
+        console.log(stickers)
+        stickers.title = '';
+        this.#boardModell
+            .add(stickers)
+            .then(() =>
+                this.#boardView.renderList(this.#boardModell.stickers))
+    }
+
+    edit(id, value) {
+        console.log('edit')
+        this.#boardModell
+          .update(id, value)
+          .then(() => this.#boardView.renderList(this.#boardModell.stickers));
+      };
+}
