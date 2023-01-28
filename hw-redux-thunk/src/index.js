75bd455e6ed81ae198f0bfcdbfa7122408e435fc
Asiia <110309169+Asiiaa@users.noreply.github.com>
2023-01-28 13:04:54 +0200

    Add files via upload

---
+++ hw-redux-thunk/src/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/system';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>
+);
